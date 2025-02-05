3e28d0a1d87fbfdfa30958b5229948ed62a4f99d
chesterosoronyas <conyasetia@gmail.com>
2025-02-05 14:44:31 +0300

    added backend graohql users

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,17 +5,24 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import helmet, { contentSecurityPolicy } from 'helmet';
 import { AppModule } from './app/app.module';
+//import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /*const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );*/
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.use(helmet({contentSecurityPolicy:false}));
+  app.enableCors();
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application playground is running on: http://localhost:${port}/graphql`
   );
 }
 
